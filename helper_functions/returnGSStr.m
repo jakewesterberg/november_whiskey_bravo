4ae9e57b334276046fcd7137298c39c48ee35256
Jake Westerberg <jakewesterberg@gmail.com>
2026-03-20 15:12:24 +0100

    ninjake current

--- helper_functions/returnGSStr.m
+++ helper_functions/returnGSStr.m
@@ -1,7 +1,11 @@
 function val = returnGSStr(dat, ii, jj)
 
-    temp_array_1 = strtrim(split(dat{ii}, ';'));
+temp_array_1 = strtrim(split(dat{ii}, ';'));
+if ~exist('jj', 'var')
+    val = temp_array_1;
+else
     val = temp_array_1{jj};
     clear temp_array_1
+end
 
 end
